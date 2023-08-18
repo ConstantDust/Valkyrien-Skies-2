68a6f2e6dba456ce035493a8c73e53e27bc66d14
ConstantDust <Carteryackovich@gmail.com>
2023-08-18 02:11:13 -0400

    Revert to old entity renderer
    lolz

--- common/src/main/kotlin/org/valkyrienskies/mod/client/VSPhysicsEntityRenderer.kt
+++ common/src/main/kotlin/org/valkyrienskies/mod/client/VSPhysicsEntityRenderer.kt
@@ -1,37 +1,49 @@
 package org.valkyrienskies.mod.client
 
 import com.mojang.blaze3d.vertex.PoseStack
+import net.minecraft.client.Minecraft
+import net.minecraft.client.renderer.ItemBlockRenderTypes
 import net.minecraft.client.renderer.MultiBufferSource
-import net.minecraft.client.renderer.RenderType
 import net.minecraft.client.renderer.entity.EntityRenderer
 import net.minecraft.client.renderer.entity.EntityRendererProvider
+import net.minecraft.client.renderer.texture.OverlayTexture
+import net.minecraft.core.BlockPos
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.inventory.InventoryMenu
-import org.joml.Quaternionf
+import net.minecraft.world.level.block.RenderShape.INVISIBLE
+import org.valkyrienskies.mod.common.ValkyrienSkiesMod
 import org.valkyrienskies.mod.common.entity.VSPhysicsEntity
+import net.minecraft.util.RandomSource
+import org.joml.Quaternionf
+import javax.swing.text.html.parser.DTDConstants.MODEL
 
-class VSPhysicsEntityRenderer(context: EntityRendererProvider.Context) :
-    EntityRenderer<VSPhysicsEntity>(context) {
-
-    private val model: VSTestEntityModel<VSPhysicsEntity>
-
-    init {
-        shadowRadius = 0.5f
-        model = VSTestEntityModel(context.bakeLayer(VSTestEntityModel.LAYER_LOCATION))
-    }
-
+class VSPhysicsEntityRenderer(context: EntityRendererProvider.Context) : EntityRenderer<VSPhysicsEntity>(context) {
     override fun render(
         fallingBlockEntity: VSPhysicsEntity, f: Float, partialTick: Float, poseStack: PoseStack,
         multiBufferSource: MultiBufferSource, i: Int
     ) {
-
+        val blockState = ValkyrienSkiesMod.TEST_SPHERE.defaultBlockState()
+        if (blockState.renderShape != MODEL) {
+            return
+        }
+        val level = fallingBlockEntity.level()
+        if (blockState === level.getBlockState(
+                fallingBlockEntity.blockPosition()
+            ) || blockState.renderShape == INVISIBLE
+        ) {
+            return
+        }
         poseStack.pushPose()
+        val blockPos = BlockPos.containing(fallingBlockEntity.x, fallingBlockEntity.boundingBox.maxY, fallingBlockEntity.z)
         poseStack.mulPose(Quaternionf(fallingBlockEntity.getRenderRotation(partialTick)))
         poseStack.translate(-0.5, -0.5, -0.5)
-        val buffer = multiBufferSource.getBuffer(RenderType.cutout())
-
-        model.renderToBuffer(poseStack, buffer, 1, 1, 1.0F, 1.0F, 1.0F, 1.0F)
-
+        val blockRenderDispatcher = Minecraft.getInstance().blockRenderer
+        blockRenderDispatcher.modelRenderer.tesselateBlock(
+            level, blockRenderDispatcher.getBlockModel(blockState), blockState, blockPos, poseStack,
+            multiBufferSource.getBuffer(
+                ItemBlockRenderTypes.getMovingBlockRenderType(blockState)
+            ), false, RandomSource.create(), blockState.getSeed(BlockPos.ZERO), OverlayTexture.NO_OVERLAY
+        )
         poseStack.popPose()
         super.render(fallingBlockEntity, f, partialTick, poseStack, multiBufferSource, i)
     }
